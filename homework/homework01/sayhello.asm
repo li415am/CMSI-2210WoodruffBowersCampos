64dfe8aefb6302152e1cc0897f708644b0ace741
Brian Bowers <bmbowers22@gmail.com>
2023-09-01 16:02:02 -0700

    adding lines before and after

--- homework/homework01/sayhello.asm
+++ homework/homework01/sayhello.asm
@@ -1,14 +1,14 @@
 global      _start                   ; on Linux this should be "_start"
 
          section     .text
-_start:   mov         rax, 0x02000004         ; system call for write [Linux should be "_start"]
+_start:   mov         rax, 1         ; system call for write [Linux should be "_start"]
          mov         rdi, 1                  ; file handle 1 is stdout
          mov         rsi, message            ; address of string to output
-         mov         rdx, 13                 ; number of bytes
+         mov         rdx, 15                 ; number of bytes
          syscall                             ; invoke operating system to do the write
          mov         rax, 60         ; system call for exit [Linux use "60"]
          xor         rdi, rdi                ; exit code 0
          syscall                             ; invoke operating system to exit
 
          section     .data
-message: db          "Hello, World", 10
+message: db          10, "Hello, World", 10, 10
