2401a55b34a1320264ae844d3f6a3f9db4da5c70
Brian Bowers <bmbowers22@gmail.com>
2023-09-13 18:05:22 -0700

    start

---
+++ classwork/classwork03/classwork3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char*argv[]){
+
+    char alphabet = {'a', 'b', 'c', 'd', 'e'};
+    char natoAlp = {'aplha', 'bravo', 'charlie', 'delta', 'echo'};
+    char userIn[100];
+    int index = 0;
+
+
+    printf("Enter a word:" /n);
+    scanf("%s", &userIn);
+    printf("%s", userIn);
+
+
+
+    //for (int i = 0; i < sizeof(alphabet)){
+
+    //}
+
+
+}
