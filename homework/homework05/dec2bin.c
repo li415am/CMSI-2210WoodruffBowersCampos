87e05e59dbcdd0f6c154316fd6f287b8dadcb682
Brian Bowers <bmbowers22@gmail.com>
2023-10-24 22:40:23 -0700

    dec2bin done

---
+++ homework/homework05/dec2bin.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    unsigned int num;
+    do{
+    printf("Please enter an unsigned base-10 number less than or equal to 4,294,967,295:\n");
+    scanf("%u", &num);
+    } while (num < 0);
+    
+    int index = 31;
+    char binary[32];
+
+    for (int i = 0; i < 32; i++) {
+        binary[i] = '0';
+    }
+
+    while (num > 0){
+        if (num == 1){
+            binary[index] = '1';
+            num = 0;
+        }
+        if (num%2 == 1){
+            binary[index] = '1';
+        }
+        index -= 1;
+        num /= 2;
+    }
+
+    printf("%s", binary);
+    return 0;
+} 
