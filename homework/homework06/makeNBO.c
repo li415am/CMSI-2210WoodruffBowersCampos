1ccf648a6ef8a67ed9945bfd04f1df5a7892680a
Brian Bowers <bmbowers22@gmail.com>
2023-11-28 18:36:24 -0800

    does it need to be two files

--- homework/homework06/makeNBO.c
+++ homework/homework06/makeNBO.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+
 
 int makeNBO(int num){
 
@@ -11,18 +12,33 @@ int makeNBO(int num){
         return nbo;
     }
     return num;
-    // printf("%d\n", num);
-    // int bytes = sizeof(num);
-    // printf("%d\n", bytes);
-    // int count = (bytes + 5) / 4;
-    // printf("%d\n", count);
-    // for (int i = 0; i < 1; i++){
-    //      printf("%02x", ptr[i]);
-    // }
+}
+
+void print_bytes(int number){
+    char* ptr = (char*)&number;
+    for (int i=0; i < 4; ++i){
+        printf("%02x ", ptr[i]);
+    }
 }
 
 int main(){
+    printf("        1  2  3  4    byte\n");
+
     int test1 = makeNBO(0x12345678);
-    printf("Before: %d\n", 0x12345678);
-    printf("After:  %d", test1);
+    printf("\nBefore: ");
+    print_bytes(0x12345678);
+    printf("\nAfter:  ");
+    print_bytes(test1);
+    printf("\n");
+
+    int test2 = makeNBO(0x11223344);
+    printf("\nBefore: ");
+    print_bytes(0x11223344);
+    printf("\nAfter:  ");
+    print_bytes(test2);
+    printf("\n");
+
 }
+
+
+
