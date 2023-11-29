b39ae9400a86893b099fcc2753dd4e2cbb51b46c
Brian Bowers <bmbowers22@gmail.com>
2023-11-28 23:23:40 -0800

    done

--- homework/homework06/whichEndIsUp.c
+++ homework/homework06/whichEndIsUp.c
@@ -5,10 +5,10 @@ int main(){
     char* ptr = (char*)&num;
 
     if (ptr[0] == 0x12){
-                printf("This system is big endian");
+                printf("This system is big endian\n");
     } 
     else if (ptr[0] == 0x79){
-        printf("This system is little endian");
+        printf("This system is little endian\n");
     }
 
     return 0;
